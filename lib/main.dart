477d13b7372f2e3bec5f79a6b8b7afba1e875ed1
Christian Aranas <aranaschristianlouise@gmail.com>
2024-04-05 14:59:23 +0800

    Added initial implementation of habit rendering
    This allows the deletion of habits from a list that is then widget rendered.
    The `HabitListPage` is refactored to a `StatefulWidget`.
    Creation of Habit class for storing habit data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class Habit {
+  final String name;
+  final List<List<Int>> recurrances;
+
+  Habit({required this.name, this.recurrances = const []});
 }
 
 class NavDestinations {
@@ -187,45 +196,69 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class HabitListPage extends StatelessWidget {
+class HabitListPage extends StatefulWidget {
   const HabitListPage({super.key});
 
   @override
+  State<HabitListPage> createState() => _HabitListPageState();
+}
+
+class _HabitListPageState extends State<HabitListPage> {
+  // TODO Replace with actual data
+  final habitsObjPlaceholder = [
+    Habit(name: 'Exercise'),
+    Habit(name: 'Write one code commit'),
+    Habit(name: 'Drink water when peckish'),
+    Habit(name: 'Write one Obsidian entry'),
+    Habit(name: 'Finish one lesson'),
+  ];
+  @override
   Widget build(BuildContext context) {
-    // TODO Replace with actual data
-    final habitsPlaceholder = [
-      "Exercise",
-      "Write one code commit",
-      "Drink water when peckish",
-      "Write one Obsidian entry",
-      "Finish one lesson"
-    ];
+    void deleteHabit(Habit habit) {
+      setState(() {
+        print(habitsObjPlaceholder.remove(habit));
+        print(habitsObjPlaceholder);
+      });
+    }
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: ListView(
+      child: Column(
         children: [
-          Text(
-            "Your current habits",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          Container(
+            child: Text(
+              "Your current habits",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
           ),
-          ...habitsPlaceholder.map((e) => ListTile(
-              title: Text(e),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.white),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(4)))),
-                    onPressed: () => print("Button pressed"),
-                    icon: Icon(Icons.delete_forever),
-                  )
-                ],
-              )))
+          Expanded(
+            child: ListView(
+              children: [
+                ...habitsObjPlaceholder.map((e) => ListTile(
+                    tileColor: Theme.of(context).colorScheme.secondaryContainer,
+                    title: Text(e.name),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4)))),
+                          onPressed: () => deleteHabit(e),
+                          icon: Icon(Icons.delete_forever),
+                        )
+                      ],
+                    ))),
+                SizedBox(
+                  height: 56 + 16,
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );

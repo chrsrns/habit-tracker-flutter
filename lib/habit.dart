7484827b0843aca1e9c31cfc9bd2159031caad18
Christian Aranas <aranaschristianlouise@gmail.com>
2024-04-11 14:58:25 +0800

    Modified `HabitListPageState`
    Added a loading screen in case the resolution of its future takes longer.
    Added the `renderListData` method to flatten the code.
    Added a delay to the future to simulate IO delays.

--- lib/habit.dart
+++ lib/habit.dart
@@ -15,18 +15,32 @@ class _HabitListPageState extends State<HabitListPage> {
   Future<sqlite.ResultSet> _habitData =
       Future.value(sqlite.ResultSet([], [], []));
 
+  bool _isLoading = true;
+
   @override
   void initState() {
     super.initState();
-    _habitData = DatabaseHelper.retrieveHabits();
+    _habitData = Future.delayed(
+            Duration(seconds: 0), (() => DatabaseHelper.retrieveHabits()))
+        .whenComplete(() => setState(() {
+              _isLoading = false;
+              print(_isLoading);
+            }));
   }
 
   @override
   Widget build(BuildContext context) {
     void deleteHabit(String habit) {
       setState(() {
+        _isLoading = true;
+        print(_isLoading);
         DatabaseHelper.deleteHabitByName(habit);
-        _habitData = DatabaseHelper.retrieveHabits();
+        _habitData = Future.delayed(
+                Duration(seconds: 0), (() => DatabaseHelper.retrieveHabits()))
+            .whenComplete(() => setState(() {
+                  _isLoading = false;
+                  print(_isLoading);
+                }));
       });
     }
 
@@ -44,80 +58,91 @@ class _HabitListPageState extends State<HabitListPage> {
             future: _habitData,
             builder: (BuildContext context,
                 AsyncSnapshot<sqlite.ResultSet> snapshot) {
-              List<Widget> children;
-              if (snapshot.hasData) {
-                var data = snapshot.data;
-                if (data != null)
-                  children = <Widget>[
-                    ...data.map((e) => Card(
-                          elevation: 5,
-                          child: Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: ListTile(
-                                // tileColor:
-                                //     Theme.of(context).colorScheme.secondaryContainer,
-                                title: Text(e['name']),
-                                trailing: material.Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    IconButton(
-                                      style: ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStateProperty.all(
-                                                  Colors.white),
-                                          shape: MaterialStateProperty.all<
-                                                  RoundedRectangleBorder>(
-                                              RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          4)))),
-                                      onPressed: () => deleteHabit(e['name']),
-                                      icon: Icon(Icons.delete_forever),
-                                    )
-                                  ],
-                                )),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 56 + 16,
-                    )
-                  ];
-                else
-                  children = [];
+              Widget childWidget;
+              if (snapshot.hasData && !_isLoading) {
+                childWidget =
+                    ListView(children: renderListData(snapshot, deleteHabit));
               } else if (snapshot.hasError) {
-                children = <Widget>[
-                  const Icon(
-                    Icons.error_outline,
-                    color: Colors.red,
-                    size: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('Error: ${snapshot.error}'),
-                  ),
-                ];
+                childWidget = Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Error: ${snapshot.error}'),
+                    ),
+                  ],
+                ));
               } else {
-                children = const <Widget>[
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: CircularProgressIndicator(),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: Text('Awaiting result...'),
-                  ),
-                ];
+                childWidget = Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text('Awaiting result...'),
+                    ),
+                  ],
+                ));
               }
-              return Expanded(
-                child: ListView(
-                  children: children,
-                ),
-              );
+              return Expanded(child: childWidget);
             },
           ),
         ],
       ),
     );
+  }
+
+  List<Widget> renderListData(AsyncSnapshot<sqlite.ResultSet> snapshot,
+      void deleteHabit(String habit)) {
+    var data = snapshot.data;
+    var result;
+    if (data != null)
+      result = <Widget>[
+        ...data.map((e) {
+          final buttonStyle = ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.white),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4))));
+          final trailingButtonRow = material.Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                style: buttonStyle,
+                onPressed: () => deleteHabit(e['name']),
+                icon: Icon(Icons.delete_forever),
+              )
+            ],
+          );
+          return Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: ListTile(
+                  // tileColor:
+                  //     Theme.of(context).colorScheme.secondaryContainer,
+                  title: Text(e['name']),
+                  trailing: trailingButtonRow),
+            ),
+          );
+        }),
+        SizedBox(
+          height: 56 + 16,
+        )
+      ];
+    else
+      result = [];
+    return result;
   }
 }

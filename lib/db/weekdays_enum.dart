60ed577212e7ad6ab6bd48d6d912298c5b1e1db1
Christian Aranas <aranaschristianlouise@gmail.com>
2024-04-28 17:36:08 +0800

    Added enum `Weekdays`
    This is to enforce the indexing and values of each weekdays.

---
+++ lib/db/weekdays_enum.dart
@@ -0,0 +1,12 @@
+enum Weekday {
+  monday("Monday"),
+  tuesday("Tuesday"),
+  wednesday("Wednesday"),
+  thursday("Thursday"),
+  friday("Friday"),
+  saturday("Saturday"),
+  sunday("Sunday");
+
+  const Weekday(this.label);
+  final String label;
+}

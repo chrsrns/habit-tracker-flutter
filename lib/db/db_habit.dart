5989ab59e42d7624da58f3253d79e6f3e69aa5a9
Christian Aranas <aranaschristianlouise@gmail.com>
2024-05-04 14:15:49 +0800

    Changed all database classes as immutable
    Both `Habit` and `TimeRange` classes are now marked as immutable.
    Removed functions that modify the `final` fields,

--- lib/db/db_habit.dart
+++ lib/db/db_habit.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:cohabit/db/db_time_range.dart';
+import 'package:flutter/foundation.dart';
 
+@immutable
 class Habit {
   final String name;
-  Map<int, List<TimeRange>> recurrances = {};
+  final Map<int, List<TimeRange>> recurrances;
 
   Habit({required this.name, Map<int, List<TimeRange>>? recurrances})
       : recurrances = recurrances ?? {};
--- lib/db/db_time_range.dart
+++ lib/db/db_time_range.dart
@@ -1,42 +1,30 @@
-class TimeRange {
-  late int _startHour;
-  late int _startMinute;
+import 'package:flutter/foundation.dart';
 
-  late int _endHour;
-  late int _endMinute;
+@immutable
+class TimeRange {
+  final int _startHour;
+  final int? _startMinute;
+
+  final int? _endHour;
+  final int? _endMinute;
 
   TimeRange(
-      {required int startHour,
-      int? startMinute,
-      int? endHour,
-      int? endMinute}) {
-    this._startHour = startHour;
-    this._startMinute = startMinute ?? 0;
-    this._endHour = endHour ?? startHour;
-    this._endMinute = endMinute ?? startMinute ?? 0;
-  }
+      {required int startHour, int? startMinute, int? endHour, int? endMinute})
+      : _startHour = startHour,
+        _startMinute = startMinute,
+        _endHour = endHour,
+        _endMinute = endMinute;
 
   int get startHour => _startHour;
-  int get startMinute => _startMinute;
-  int get endHour => _endHour;
-  int get endMinute => _endMinute;
+  int get startMinute => _startMinute ?? 0;
+  int get endHour => _endHour ?? _startHour;
+  int get endMinute => _endMinute ?? _startMinute ?? 0;
 
   String get startTime => "$_startHour:$_startMinute:00";
   String get endTime => "$_endHour:$_endMinute:00";
 
-  void updateTime(
-      {required int startHour,
-      int? startMinute,
-      int? endHour,
-      int? endMinute}) {
-    this._startHour = startHour;
-    this._startMinute = startMinute ?? 0;
-    this._endHour = endHour ?? startHour;
-    this._endMinute = endMinute ?? startMinute ?? 0;
-  }
-
   @override
   String toString() {
-    return "$start_hour:$start_minute:00";
+    return "$_startHour:$_startMinute:00|$_endHour:$_endMinute:00";
   }
 }

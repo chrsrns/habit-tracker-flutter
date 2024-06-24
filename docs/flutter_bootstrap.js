54f38e8bddfb9b17e4769ef0f2da59f0518ca699
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-06-24 15:01:15 +0800

    Update github pages assets

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2394044778"
+    serviceWorkerVersion: "3488293558"
   }
 });

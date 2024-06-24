fdff3ae343d788892df3675e80af71148e023674
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-06-24 10:55:16 +0800

    Changes in `habit_list_page.dart`
    Refactor type `_habitData` to `sqlite.ResultSet`.
    Add a message when the _habitData is empty.

--- lib/habit_list_page.dart
+++ lib/habit_list_page.dart
@@ -20,8 +20,7 @@ class HabitListPage extends StatefulWidget {
 
 class _HabitListPageState extends State<HabitListPage> {
   // TODO This no longer needs to be a Future. Should be replaced.
-  Future<sqlite.ResultSet?> _habitData =
-      Future.value(sqlite.ResultSet([], [], []));
+  sqlite.ResultSet? _habitData;
 
   Habit? ongoingHabit;
   Cron cron = Cron();
@@ -33,7 +32,7 @@ class _HabitListPageState extends State<HabitListPage> {
   void initState() {
     super.initState();
     _habitsSortedSubscription = DatabaseHelper.habitsSorted.listen((event) {
-      _habitData = Future.value(event);
+      _habitData = event;
     });
 
     _ongoingHabitSubscription = DatabaseHelper.ongoingHabit.listen((event) {
@@ -72,186 +71,168 @@ class _HabitListPageState extends State<HabitListPage> {
             ),
           ),
           Divider(),
-          FutureBuilder<sqlite.ResultSet?>(
-            future: _habitData,
-            builder: (BuildContext context,
-                AsyncSnapshot<sqlite.ResultSet?> snapshot) {
-              Widget childWidget;
-              print("snapshot.hasData: ${snapshot.hasData}");
-              if (snapshot.hasData) {
+          () {
+            Widget childWidget;
+            if (_habitData != null) {
+              final habitData = _habitData!;
+              if (habitData.length <= 0)
+                childWidget = Center(
+                    child: Padding(
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: Text("You do not have any habits here, yet."),
+                ));
+              else
                 childWidget =
-                    ListView(children: renderListData(snapshot, deleteHabit));
-              } else if (snapshot.hasError) {
-                childWidget = Center(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Icon(
-                      Icons.error_outline,
-                      color: Colors.red,
-                      size: 60,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Text('Error: ${snapshot.error}'),
-                    ),
-                  ],
-                ));
-              } else {
-                childWidget = Center(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      width: 60,
-                      height: 60,
-                      child: CircularProgressIndicator(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 16),
-                      child: Text('Awaiting result...'),
-                    ),
-                  ],
-                ));
-              }
-              return Expanded(child: childWidget);
-            },
-          ),
+                    ListView(children: renderListData(habitData, deleteHabit));
+            } else {
+              childWidget = Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  ),
+                ],
+              ));
+            }
+            return Expanded(child: childWidget);
+          }(),
         ],
       ),
     );
   }
 
-  List<Widget> renderListData(AsyncSnapshot<sqlite.ResultSet?> snapshot,
-      void deleteHabit(String habit)) {
-    var data = snapshot.data;
+  List<Widget> renderListData(
+      sqlite.ResultSet snapshot, void deleteHabit(String habit)) {
+    var data = snapshot;
     var habits = HashSet<String>();
     var result = <Widget>[];
-    if (data != null) {
-      for (var habitRow in data) {
-        var habitName = habitRow['${TableHabitRecurrance.habit_fr}'] as String;
-        var weekday = habitRow['${TableHabitRecurrance.weekday_id_fr}'] as int;
-        var startHour =
-            habitRow['${TableHabitRecurrance.start_hour_fr}'] as int;
-        var startMinute =
-            habitRow['${TableHabitRecurrance.start_minute_fr}'] as int;
-        var endHour = habitRow['${TableHabitRecurrance.end_hour_fr}'] as int;
-        var endMinute =
-            habitRow['${TableHabitRecurrance.end_minute_fr}'] as int;
-        if (habits.contains(habitName)) continue;
+    for (var habitRow in data) {
+      var habitName = habitRow['${TableHabitRecurrance.habit_fr}'] as String;
+      var weekday = habitRow['${TableHabitRecurrance.weekday_id_fr}'] as int;
+      var startHour = habitRow['${TableHabitRecurrance.start_hour_fr}'] as int;
+      var startMinute =
+          habitRow['${TableHabitRecurrance.start_minute_fr}'] as int;
+      var endHour = habitRow['${TableHabitRecurrance.end_hour_fr}'] as int;
+      var endMinute = habitRow['${TableHabitRecurrance.end_minute_fr}'] as int;
+      if (habits.contains(habitName)) continue;
 
-        habits.add(habitName);
-        final buttonStyle = ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.white),
-            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4))));
-        final trailingButtonRow = material.Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              style: buttonStyle,
-              onPressed: () {
-                DatabaseHelper.retrieveHabit(habitName).then((habit) {
-                  if (habit != null) {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return NewHabitDialog(habit: habit);
-                        });
-                  }
-                });
-              },
-              icon: Icon(Icons.edit),
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            IconButton(
-              style: buttonStyle,
-              onPressed: () => deleteHabit(habitName),
-              icon: Icon(Icons.delete_forever),
-            )
-          ],
-        );
-        late Widget habitText;
-        late Card paddedChip;
-        late Color cardColor;
-        if (ongoingHabit?.name == habitName) {
-          habitText = IntrinsicWidth(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(style: TextStyle(fontSize: 12), "Ongoing Habit"),
-              Text(style: TextStyle(fontWeight: FontWeight.bold), habitName),
-            ],
-          ));
-          paddedChip = Card(
-            margin: EdgeInsets.all(0),
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Wrap(
-                spacing: 4,
-                crossAxisAlignment: WrapCrossAlignment.end,
-                key: ValueKey(habitName),
-                children: [
-                  Text(style: TextStyle(fontSize: 12), "Habit ends in: "),
-                  Text(
-                      style: TextStyle(fontSize: 12),
-                      TimeOfDay(hour: endHour, minute: endMinute)
-                          .format(context)),
-                ],
-              ),
-            ),
-          );
-          cardColor = Theme.of(context).colorScheme.tertiaryContainer;
-        } else {
-          habitText = Text(habitName);
-          paddedChip = Card(
-            margin: EdgeInsets.all(0),
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Wrap(
-                spacing: 4,
-                crossAxisAlignment: WrapCrossAlignment.end,
-                key: ValueKey(habitName),
-                children: [
-                  Text(style: TextStyle(fontSize: 12), "Next Start: "),
-                  Text(
-                      style: TextStyle(fontSize: 12),
-                      Weekday.fromInt(weekday).label),
-                  Text(
-                      style: TextStyle(fontSize: 12),
-                      TimeOfDay(hour: startHour, minute: startMinute)
-                          .format(context)),
-                ],
-              ),
-            ),
-          );
-          cardColor = Theme.of(context).colorScheme.surface;
-        }
-        result.add(Card(
-          color: cardColor,
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 4, bottom: 4),
-                      child: habitText,
-                    ),
-                    paddedChip
-                  ],
-                ),
-                trailing: trailingButtonRow),
+      habits.add(habitName);
+      final buttonStyle = ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.white),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(4))));
+      final trailingButtonRow = material.Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            style: buttonStyle,
+            onPressed: () {
+              DatabaseHelper.retrieveHabit(habitName).then((habit) {
+                if (habit != null) {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return NewHabitDialog(habit: habit);
+                      });
+                }
+              });
+            },
+            icon: Icon(Icons.edit),
           ),
+          SizedBox(
+            width: 8,
+          ),
+          IconButton(
+            style: buttonStyle,
+            onPressed: () => deleteHabit(habitName),
+            icon: Icon(Icons.delete_forever),
+          )
+        ],
+      );
+      late Widget habitText;
+      late Card paddedChip;
+      late Color cardColor;
+      if (ongoingHabit?.name == habitName) {
+        habitText = IntrinsicWidth(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(style: TextStyle(fontSize: 12), "Ongoing Habit"),
+            Text(style: TextStyle(fontWeight: FontWeight.bold), habitName),
+          ],
         ));
+        paddedChip = Card(
+          margin: EdgeInsets.all(0),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Wrap(
+              spacing: 4,
+              crossAxisAlignment: WrapCrossAlignment.end,
+              key: ValueKey(habitName),
+              children: [
+                Text(style: TextStyle(fontSize: 12), "Habit ends in: "),
+                Text(
+                    style: TextStyle(fontSize: 12),
+                    TimeOfDay(hour: endHour, minute: endMinute)
+                        .format(context)),
+              ],
+            ),
+          ),
+        );
+        cardColor = Theme.of(context).colorScheme.tertiaryContainer;
+      } else {
+        habitText = Text(habitName);
+        paddedChip = Card(
+          margin: EdgeInsets.all(0),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Wrap(
+              spacing: 4,
+              crossAxisAlignment: WrapCrossAlignment.end,
+              key: ValueKey(habitName),
+              children: [
+                Text(style: TextStyle(fontSize: 12), "Next Start: "),
+                Text(
+                    style: TextStyle(fontSize: 12),
+                    Weekday.fromInt(weekday).label),
+                Text(
+                    style: TextStyle(fontSize: 12),
+                    TimeOfDay(hour: startHour, minute: startMinute)
+                        .format(context)),
+              ],
+            ),
+          ),
+        );
+        cardColor = Theme.of(context).colorScheme.surface;
       }
+      result.add(Card(
+        color: cardColor,
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: ListTile(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4, bottom: 4),
+                    child: habitText,
+                  ),
+                  paddedChip
+                ],
+              ),
+              trailing: trailingButtonRow),
+        ),
+      ));
     }
     result.add(SizedBox(
       height: 56 + 16,
